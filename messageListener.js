d5ea82ab256897009af2f4258abb168a08de8b2a
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-09-24 05:11:20 -0600

    Make Minori more sensitive

--- messageListener.js
+++ messageListener.js
@@ -2,9 +2,9 @@ const commandMap = require('./commands/map');
 const mention = require('./commands/mention');
 const unknown = require('./commands/unknown');
 module.exports = function(message){
-   var arguments = message.cleanContent.toLowerCase().trim().split(' ').filter(function(entry) { return entry != ''; });;
+   var arguments = message.cleanContent.trim().split(' ').filter(function(entry) { return entry != ''; });;
    var command = arguments[1];
-   if (arguments[0] === 'minori') {
+   if (arguments[0].toLowerCase() === 'minori') {
      if (arguments[1]) {
        if (commandMap[arguments[1]]) {
          if (arguments.length > 2) {
