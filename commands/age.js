4b7bcf987d9506934e4ee9b22aee4ac3081a0baf
Caleb <caleb@Calebs-MacBook-Pro.local>
2017-09-24 14:20:07 -0600

    bugfix age

--- commands/age.js
+++ commands/age.js
@@ -1,7 +1,7 @@
 const DateDiff = require('date-diff');
 const birthday = new Date('2017-09-24T10:10:08Z');
 module.exports = function(message){
-  let dif = new DateDiff(new Date(),birthday);
-  console.log(birthday, new Date().getTime());
+  var nowUTC = new Date(new Date().getTime());
+  let dif = new DateDiff(nowUTC,birthday);
   message.channel.send('I am '+Math.floor(dif.years())+" Years, "+dif.months() % 12+" Months, and "+dif.days() % 365+" days old.");
 }
